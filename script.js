fd3498884d72d75658f9ed03de6d4885a63ce1f4
Reboot Tech <tech@reboot.no>
2025-10-16 15:25:19 +0200

    feat: update layout and styles, add contact page, add assets

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+      if (window.scrollY > 50) {
+        header.classList.add("header--scrolled");
+      } else {
+        header.classList.remove("header--scrolled");
+}});
