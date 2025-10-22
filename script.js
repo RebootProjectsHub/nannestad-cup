8ed4e4ce23354cb3f6c71aebf6ee4bd6d79a5980
Reboot Tech <tech@reboot.no>
2025-10-22 16:26:06 +0200

    feat: update pages layout and styles

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Header turns blured white when page is scrolled
 window.addEventListener("scroll", () => {
   const header = document.querySelector(".header");
       if (window.scrollY > 50) {
@@ -6,13 +7,17 @@ window.addEventListener("scroll", () => {
         header.classList.remove("header--scrolled");
 }});
 
-const toggles = document.querySelectorAll('.rules__toggle');
-toggles.forEach(btn => {
-btn.addEventListener('click', () => {
-const content = document.getElementById(btn.getAttribute('aria-controls'));
-const expanded = btn.getAttribute('aria-expanded') === 'true';
-btn.setAttribute('aria-expanded', !expanded);
-content.hidden = expanded;
-btn.textContent = expanded ? '+' : '–';
-});
-});
+// Get current year on footer copyright
+document.getElementById("footer-year").textContent = new Date().getFullYear();
+
+
+// const toggles = document.querySelectorAll('.rules__toggle');
+// toggles.forEach(btn => {
+// btn.addEventListener('click', () => {
+// const content = document.getElementById(btn.getAttribute('aria-controls'));
+// const expanded = btn.getAttribute('aria-expanded') === 'true';
+// btn.setAttribute('aria-expanded', !expanded);
+// content.hidden = expanded;
+// btn.textContent = expanded ? '+' : '–';
+// });
+// });
