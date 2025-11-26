21d6915ba2d0c1eab64016e01fde43c2f1ae22da
Reboot Tech <tech@reboot.no>
2025-11-26 19:33:38 +0100

    feat: add mobile menu

--- script.js
+++ script.js
@@ -21,3 +21,43 @@ document.getElementById("footer-year").textContent = new Date().getFullYear();
 // btn.textContent = expanded ? '+' : '–';
 // });
 // });
+
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+   const toggle = document.getElementById(toggleId),
+         nav = document.getElementById(navId)
+
+   toggle.addEventListener('click', () =>{
+       // Add show-menu class to nav menu
+       nav.classList.toggle('show-menu')
+
+       // Add show-icon to show and hide the menu icon
+       toggle.classList.toggle('show-icon')
+   })
+}
+
+showMenu('nav-toggle','nav-menu')
+
+document.querySelectorAll('.dropdown__item').forEach(item => {
+  const link = item.querySelector('.nav__link');
+  const submenu = item.querySelector('.dropdown__menu');
+  const arrow = item.querySelector('.dropdown__arrow');
+
+  link.addEventListener('click', (e) => {
+    // just mobile
+    if (window.innerWidth <= 1118) {
+      e.preventDefault();
+
+      // switch state
+      item.classList.toggle('open');
+
+      if (item.classList.contains('open')) {
+        submenu.style.maxHeight = submenu.scrollHeight + 'px';
+        arrow.style.transform = 'rotate(180deg)';
+      } else {
+        submenu.style.maxHeight = null;
+        arrow.style.transform = 'rotate(0deg)';
+      }
+    }
+  });
+});
